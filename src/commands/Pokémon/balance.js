5a804f24315ce43497bda466dbd4c66a9fbfb7e5
Shadeslayer10 <95581967+Shadeslayer10@users.noreply.github.com>
2022-03-08 19:34:38 +0530

    Add files via upload

--- src/commands/Pokémon/balance.js
+++ src/commands/Pokémon/balance.js
@@ -1,1 +1,27 @@
+const { MessageEmbed } = require('discord.js');
+const user = require('../../models/profile.js');
 
+module.exports = {
+  name: 'bal',
+  aliases: ['balance', 'coins'],
+  category: 'Pokémon',
+  execute: async (client, message, args) => {
+    const data = await user.findOne({ Id: message.author.id });
+    if (!data) return message.channel.send(client.config.errors.userNotStarted);
+    const embed = new MessageEmbed()
+      .setTitle(`${message.author.username}'s balance`)
+      .setThumbnail(message.author.displayAvatarURL())
+      .setColor(client.config.colors.default)
+      .setDescription(
+        ` ${
+          client.config.emotes.gold
+        } Gold : \`${data.bal.gold.toLocaleString()}\` \n${
+          client.config.emotes.gems
+        } Gems : \`${data.bal.gems.toLocaleString()}\` \n${
+          client.config.emotes.redeems
+        } Redeems : \`${data.bal.redeems.toLocaleString()}\``
+      )
+      .setTimestamp();
+    message.channel.send({ embeds: [embed] });
+  },
+};
